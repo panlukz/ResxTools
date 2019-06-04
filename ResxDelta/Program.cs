bfc2731995ce9a7a3c1407d5e3d8828891b6bc55
lukasz <lukasz@wp.pl>
2019-06-04 17:11:14 +0200

    Initial commit

---
+++ ResxDelta/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text.RegularExpressions;
+
+namespace ResxDelta
+{
+    static class StringExtensions
+    {
+        public static string ToFlat(this string str) => Regex.Replace(str, @"[^\w]", "").ToLower();
+    }
+
+    class Program
+    {
+        static readonly ITypeResolutionService tnull = null;
+        private const string DeltaFileName = "delta.resx";
+
+        public class ResXDataNodeComparer : IEqualityComparer<ResXDataNode>
+        {
+            public bool Equals(ResXDataNode dn1, ResXDataNode dn2)
+            {
+                return dn1.Name.Equals(dn2.Name) &&
+                       dn1.GetValue(tnull).ToString().ToFlat().Equals(dn2.GetValue(tnull).ToString().ToFlat()) &&
+                       dn1.Comment.Equals(dn2.Comment);
+            }
+            public int GetHashCode(ResXDataNode obj)
+            {
+                return string.Concat(obj.Name, obj.GetValue(tnull).ToString().ToFlat(), obj.Comment).GetHashCode();
+            }
+        }
+
+        private static void ExitWithError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+            Environment.Exit(-1);
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 2) ExitWithError("Two arguments with file names have to be specified!\nusage: resxdelta newerFilePath olderFilePath");
+            if(!File.Exists(args[0])) ExitWithError($"Specified file \"{args[0]}\" does not exist!");
+            if (!File.Exists(args[1])) ExitWithError($"Specified file \"{args[1]}\" does not exist!");
+
+            using (var newResxReader = new ResXResourceReader(args[0]) {UseResXDataNodes = true})
+            using (var oldResxReader = new ResXResourceReader(args[1]) {UseResXDataNodes = true})
+            using (var deltaResxWriter = new ResXResourceWriter(DeltaFileName))
+            {
+                try
+                {
+                    var modifiedResources = newResxReader.Cast<DictionaryEntry>().Select(n => (ResXDataNode) n.Value)
+                        .Except(oldResxReader.Cast<DictionaryEntry>().Select(n2 => (ResXDataNode) n2.Value),
+                            new ResXDataNodeComparer())
+                        .ToList();
+
+                    Console.WriteLine($"{modifiedResources.Count} modified resources found!");
+
+                    if(modifiedResources.Count == 0) return;
+
+                    modifiedResources.ForEach(r => deltaResxWriter.AddResource(new ResXDataNode(
+                            r.Name,
+                            r.GetValue(tnull).ToString()) {Comment = r.Comment}));
+
+                    Console.WriteLine($"Writting to {DeltaFileName}...");
+
+                    deltaResxWriter.Generate();
+                }
+                catch (Exception e)
+                {
+                    ExitWithError(e.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ ResxMerge/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Resources;
+
+namespace ResxMerge
+{
+    class Program
+    {
+        static readonly ITypeResolutionService tnull = null;
+
+        private static void ExitWithError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+            Environment.Exit(-1);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                ExitWithError(
+                    "Two arguments with file names have to be specified!\nusage: resxmerge originalResxFilePath translatedDeltaFilePath");
+            if (!File.Exists(args[0])) ExitWithError($"Specified file \"{args[0]}\" does not exist!");
+            if (!File.Exists(args[1])) ExitWithError($"Specified file \"{args[1]}\" does not exist!");
+
+            using (var originalResxReader = new ResXResourceReader(args[0]) { UseResXDataNodes = true })
+            using (var deltaResxReader = new ResXResourceReader(args[1]) { UseResXDataNodes = true })
+            using (var outputResxWriter = new ResXResourceWriter(args[0]))
+            {
+                try
+                {
+                    var originalResources = originalResxReader.Cast<DictionaryEntry>().Select(n => (ResXDataNode) n.Value).ToList();
+                    var deltaResources = deltaResxReader.Cast<DictionaryEntry>().Select(n => (ResXDataNode) n.Value).ToList();
+
+                    Console.WriteLine($"Original resources: {originalResources.Count}");
+                    Console.WriteLine($"Delta resources: {deltaResources.Count}");
+
+                    int counter = 0;
+                    foreach (var resx in originalResources)
+                    {
+                        var match = deltaResources.FirstOrDefault(x => x.Name.Equals(resx.Name));
+                        if (match != null) counter++;
+
+                        outputResxWriter.AddResource(new ResXDataNode(
+                                resx.Name,
+                                match != null ? match.GetValue(tnull).ToString() : resx.GetValue(tnull).ToString())
+                            {Comment = resx.Comment});
+                    }
+
+                    Console.WriteLine($"Modified resources: {counter}");
+                    Console.WriteLine($"Writting to {args[0]}...");
+
+                    outputResxWriter.Generate();
+                }
+                catch (Exception e)
+                {
+                    ExitWithError(e.Message);
+                }
+            }
+        }
+    }
+}
